58f619fbc88811b96257bb2e75bb3d7e0ba4da3d
astirpe <a.stirpe@isa.it>
2014-07-16 09:49:49 +0200

    Add module sale_stock_salesagent_commissions

---
+++ sale_stock_salesagent_commissions/sale/sale_order.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2014 ISA s.r.l. (<http://www.isa.it>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from openerp.osv import fields, orm
+
+
+class sale_order(orm.Model):
+    _inherit = 'sale.order'
+
+    def _prepare_order_picking(self, cr, uid, order, context=None):
+        result = super(sale_order,self)._prepare_order_picking(cr, uid, order, context=context)
+        result.update(salesagent_id=order.salesagent_id and order.salesagent_id.id or False)
+        return result
